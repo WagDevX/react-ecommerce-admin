f6e005aebf30f663aa3a8aefd5da17200f01be99
WagDevX <waguinbk@gmail.com>
2023-06-09 02:29:08 -0300

    upload function almost done

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,6 +7,7 @@ export default function ProductForm({
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
@@ -17,8 +18,7 @@ export default function ProductForm({
     ev.preventDefault();
     const data = { title, description, price };
     if (_id) {
-      await axios.put('/api/products', {...data,_id});
-      
+      await axios.put("/api/products", { ...data, _id });
     } else {
       await axios.post("/api/products", data);
     }
@@ -26,6 +26,20 @@ export default function ProductForm({
   }
   if (goToProducts) {
     router.push("/products");
+  }
+  async function uploadeImages (ev) {
+    const files = ev.target?.files;
+    if (files.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        data.append('file', file);
+      }
+      const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: data,
+      })
+      console.log(res);
+    }
   }
   return (
     <form onSubmit={saveProduct}>
@@ -36,6 +50,28 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+      <label>Photos</label>
+      <div className="mb-2">
+        <label className="cursor-pointer w-24 h-24 text-center flex-col flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 hover:bg-gray-300 transition duration-300 ease-in">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          Upload
+          <input type="file" onChange={uploadeImages} className="hidden"></input>
+        </label>
+        {!images?.length && <div>No photos in this product</div>}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="description"
---
+++ pages/api/upload.js
@@ -0,0 +1,17 @@
+import multiparty from "multiparty";
+export default async function handle(req, res) {
+  const form = new multiparty.Form();
+  const { fields, files } = await new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      resolve({ fields, files });
+    });
+  });
+  console.log("length:", files);
+  console.log(fields);
+  res.json("ok");
+}
+
+export const config = {
+  api: { bodyParser: false },
+};
