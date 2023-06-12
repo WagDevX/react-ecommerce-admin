e8064345f358aa5179cbee15d8bf0b9b98ccd04d
WagDevX <waguinbk@gmail.com>
2023-06-12 00:21:31 -0300

    timestamps added schema

--- models/Product.js
+++ models/Product.js
@@ -7,6 +7,9 @@ const ProductSchema = new Schema( {
     images: {type:[String]},
     category: {type:mongoose.Types.ObjectId, ref: "Category"},
     properties: {type: Object}
+},{
+    timestamps: true,
+
 });
 
 export const Product = models.Products || model('Products', ProductSchema);
