812c58b90c45566505708ccd3dcc084d3c6e9bdb
WagDevX <waguinbk@gmail.com>
2023-06-26 18:51:13 -0300

    .

--- pages/settings.js
+++ pages/settings.js
@@ -9,13 +9,13 @@ const MySwal = withReactContent(Swal);
 export default function Settings() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [featuredProductId, setFeaturedProductId] = useState('');
-  const [shippingFee, setShippingFee] = useState('');
-  
+  const [featuredProductId, setFeaturedProductId] = useState("");
+  const [shippingFee, setShippingFee] = useState("");
+
   useEffect(() => {
     setIsLoading(true);
     fetchAll().then(() => {
-        setIsLoading(false);
+      setIsLoading(false);
     });
   }, []);
 
@@ -24,10 +24,10 @@ export default function Settings() {
       setProducts(res.data);
     });
     await axios.get("/api/settings?name=featuredProductId").then((res) => {
-        setFeaturedProductId(res.data.value);
+      setFeaturedProductId(res.data.value);
     });
     await axios.get("/api/settings?name=shippingFee").then((res) => {
-        setShippingFee(res.data.value);
+      setShippingFee(res.data.value);
     });
   }
 
@@ -35,18 +35,17 @@ export default function Settings() {
     setIsLoading(true);
     await axios.put("/api/settings", {
       name: "featuredProductId",
-      value: featuredProductId
-    })
+      value: featuredProductId,
+    });
     await axios.put("/api/settings", {
-        name: "shippingFee",
-        value: shippingFee
-    })
+      name: "shippingFee",
+      value: shippingFee,
+    });
     setIsLoading(false);
     await MySwal.fire({
-        title: "Settings saved!",
-        icon: "success",
+      title: "Settings saved!",
+      icon: "success",
     });
-    
   }
 
   return (
@@ -56,20 +55,43 @@ export default function Settings() {
       {isLoading && <Spinner fulllWidth={true} />}
       {!isLoading && (
         <>
-          <select value={featuredProductId} onChange={ev => setFeaturedProductId(ev.target.value)}>
-            {products.length > 0 && products.map((product) => (
-              <option value={product._id}>{product.title}</option>
-            ))}
+          <select
+            value={featuredProductId}
+            onChange={(ev) => setFeaturedProductId(ev.target.value)}
+          >
+            {products.length > 0 &&
+              products.map((product) => (
+                <option value={product._id}>{product.title}</option>
+              ))}
           </select>
           <label>Shipping price (in BRL)</label>
-          <input 
-          value={shippingFee} 
-          onChange={ev => setShippingFee(ev.target.value)} type="number" />
+          <input
+            value={shippingFee}
+            onChange={(ev) => setShippingFee(ev.target.value)}
+            type="number"
+          />
           <div>
-            <button 
-            onClick={saveSettings}
-            className="btn-primary">
-                Save settings
+            <label>Front page Banner</label>
+            <label className=" cursor-pointer w-24 h-24 text-center flex-col flex items-center justify-center text-sm gap-1 text-primary rounded-lg bg-white hover:bg-gray-300 transition duration-300 ease-in shadow-sm border border-gray-200">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                />
+              </svg>
+              Add image
+              <input type="file" onChange={() => {}} className="hidden"></input>
+            </label>
+            <button onClick={saveSettings} className="btn-primary mt-3">
+              Save settings
             </button>
           </div>
         </>
