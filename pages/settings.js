928a9d98b9a4acdde2a9a315b37b93b7c1b14e1a
WagDevX <waguinbk@gmail.com>
2023-06-26 10:22:00 -0300

    setting shipping fee added

--- pages/settings.js
+++ pages/settings.js
@@ -8,47 +8,63 @@ const MySwal = withReactContent(Swal);
 
 export default function Settings() {
   const [products, setProducts] = useState([]);
-  const [productsLoading, setProductsLoading] = useState(false);
-  const [featuredLoading, setFeaturedLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [featuredProductId, setFeaturedProductId] = useState('');
+  const [shippingFee, setShippingFee] = useState('');
   
   useEffect(() => {
-    setProductsLoading(true);
-    
-    axios.get("/api/products").then((res) => {
-      setProducts(res.data);
-      setProductsLoading(false);
-    });
-    setFeaturedLoading(true);
-    axios.get("/api/settings?name=featuredProductId").then((res) => {
-        setFeaturedProductId(res.data.value);
-        setFeaturedLoading(false);
+    setIsLoading(true);
+    fetchAll().then(() => {
+        setIsLoading(false);
     });
   }, []);
 
+  async function fetchAll() {
+    await axios.get("/api/products").then((res) => {
+      setProducts(res.data);
+    });
+    await axios.get("/api/settings?name=featuredProductId").then((res) => {
+        setFeaturedProductId(res.data.value);
+    });
+    await axios.get("/api/settings?name=shippingFee").then((res) => {
+        setShippingFee(res.data.value);
+    });
+  }
+
   async function saveSettings() {
+    setIsLoading(true);
     await axios.put("/api/settings", {
       name: "featuredProductId",
       value: featuredProductId
     })
-    MySwal.fire({
+    await axios.put("/api/settings", {
+        name: "shippingFee",
+        value: shippingFee
+    })
+    setIsLoading(false);
+    await MySwal.fire({
         title: "Settings saved!",
         icon: "success",
     });
+    
   }
 
   return (
     <Layout>
       <h1>Settings</h1>
       <label>Featured product</label>
-      {(productsLoading || featuredLoading) && <Spinner fulllWidth={true} />}
-      {(!productsLoading && !featuredLoading) && (
+      {isLoading && <Spinner fulllWidth={true} />}
+      {!isLoading && (
         <>
           <select value={featuredProductId} onChange={ev => setFeaturedProductId(ev.target.value)}>
             {products.length > 0 && products.map((product) => (
               <option value={product._id}>{product.title}</option>
             ))}
           </select>
+          <label>Shipping price (in BRL)</label>
+          <input 
+          value={shippingFee} 
+          onChange={ev => setShippingFee(ev.target.value)} type="number" />
           <div>
             <button 
             onClick={saveSettings}
