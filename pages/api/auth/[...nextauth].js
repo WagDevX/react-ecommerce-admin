f65085fa87e5bd52a656ebb0a140440f0173a839
WagDevX <waguinbk@gmail.com>
2023-06-27 18:07:56 -0300

    auth fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,6 +15,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      checks: ['none']
     }),
     // Passwordless / email sign in
   ],
